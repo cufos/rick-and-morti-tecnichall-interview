abbeaf709a20971ed9f09e418df646edfb794348
Juni <yunior.varon@yahoo.com>
2021-05-25 15:45:44 +0200

    added the navbar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,13 +1,25 @@
+import Link from 'next/link'
+// components
+import { Logo } from '../logo'
+// styles
+import styles from '../../styles/navbar.module.css'
+
 export function Navbar () {
   return (
-    <div>
-      <h2>
-        Rick And Morty
-      </h2>
-      <form>
-        <input type='text' placeholder='Search your favorite character' />
-        <button>Search</button>
-      </form>
+    <div className={styles.wraper}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>
+          <Link href='/'>
+            <a>
+              <Logo width={180} height={60} />
+            </a>
+          </Link>
+        </h1>
+        <form className={styles.form}>
+          <input type='text' placeholder='Search for a character...' />
+          <button>Search</button>
+        </form>
+      </div>
     </div>
   )
 }
